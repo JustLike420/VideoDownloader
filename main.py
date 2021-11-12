423d26efac2f4c571972a59d1dc6aa23e620b306
DimaRFF <71233113+DimaRFF@users.noreply.github.com>
2021-11-12 15:22:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data.config import BOT_TOKEN
 
+from telethon import TelegramClient
+
+api_id = 19655082
+api_hash = ''
+bot_token = BOT_TOKEN
+
+# We have to manually call "start" if we want an explicit bot token
+auth = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN)
 storage = MemoryStorage()
